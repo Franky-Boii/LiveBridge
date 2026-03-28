787e53ac5ef9406d530c7a304782574b887de881
frmulcpt025 <frmulcpt025@student.wethinkcode.co.za>
2026-03-28 12:21:55 +0200

    fix: remove invalid layout.js and finalize _app.js for production build

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -10,5 +11,13 @@ export default function MyApp({ Component, pageProps }) {
     }
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>LiveBridge | AI Communication Hub</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
